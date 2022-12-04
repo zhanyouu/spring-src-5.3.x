451a5de84442909901917db4b13297c70196bd69
zhanyou <you.zhan@xtransfer.cn>
2022-12-04 22:32:37 +0800

    spring

--- spring-demo/src/main/java/SpringTest.java
+++ spring-demo/src/main/java/SpringTest.java
@@ -1,13 +1,19 @@
 import config.UserConfig;
+import entity.Person;
 import entity.User;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringTest {
 
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
-				UserConfig.class);
-		User user = (User) context.getBean("user");
-		System.out.println(user);
+//		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+//				UserConfig.class);
+//		User user = (User) context.getBean("user");
+//		System.out.println(user);
+		ApplicationContext xmlContext = new ClassPathXmlApplicationContext("person.xml");
+		Person person = (Person) xmlContext.getBean("person");
+		System.out.println(person);
 	}
 }
